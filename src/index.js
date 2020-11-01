db8d9a7bae09294c0b506ace7bc7c6220effa2fd
Kristina Petrickova <kristina.petrickova@gmail.com>
2020-11-01 11:12:28 +0100

    Navigation drop-down show/hide when clicked

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const navLink = document.querySelectorAll('nav a');
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener('click', () => {
+    document.querySelector('nav').classList.add('nav-closed');
+  });
+}
